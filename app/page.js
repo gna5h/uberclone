684a080cec6c386f7c1d8b2f15bdd3a3c807cc91
gna5h <gg.orlanes@gmail.com>
2024-06-06 21:41:38 +0800

    fix: update app layout

--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="p-6 grid grid-cols-1 md:grid-cols-3 gap-5">
+      <div>Search</div>
+      <div className="col-span-2">Google Map</div>
+    </div>
+  );
 }
