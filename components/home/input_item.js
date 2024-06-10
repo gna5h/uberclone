6f7764785a55dfbfed234ab1f751688b9f5d45f4
gna5h <gg.orlanes@gmail.com>
2024-06-10 20:37:51 +0800

    feat: added search place autocomplete

--- components/home/input_item.js
+++ components/home/input_item.js
@@ -1,8 +1,13 @@
+"use client";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationDot, faMap } from "@fortawesome/free-solid-svg-icons";
-import React from "react";
+import React, { useState } from "react";
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 
 function InputItem({ type }) {
+  const [value, setValue] = useState(null);
+
   return (
     <div className="bg-slate-200 p-3 rounded-lg mt-3 flex items-center gap-4">
       <FontAwesomeIcon
@@ -10,10 +15,31 @@ function InputItem({ type }) {
         width={15}
         height={15}
       />
-      <input
+      {/* <input
         className="bg-transparent w-full outline-none"
         type="text"
         placeholder={type == "source" ? "Pickup Location" : "DropOff Location"}
+      /> */}
+
+      <GooglePlacesAutocomplete
+        apiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}
+        selectProps={{
+          value,
+          onChange: setValue,
+          placeholder: "Pickup Location",
+          isClearable: true,
+          className: "w-full",
+          components: {
+            DropdownIndicator: false,
+          },
+          styles: {
+            control: (provided) => ({
+              ...provided,
+              backgroundColor: "#00ffff00",
+              border: "none",
+            }),
+          },
+        }}
       />
     </div>
   );
